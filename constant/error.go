fcd5fa74ba2f4f78052686a134fbd67fa5c5eeef
Administrator <real251024@163.com>
2022-10-05 10:40:31 +0800

    feat：常量调整

---
+++ constant/error.go
@@ -0,0 +1,43 @@
+package constant
+
+// 返回1xxxx -> 请求成功
+const (
+	Success          = 10000 // 请求成功的时候的返回值
+	DataEmpty        = 10001 // 请求的数据为空时候的返回值
+	ParameterMiss    = 10002 // 请求所需的参数部分缺失
+	ParameterInvalid = 10003 // 请求的参数存在不合法的情况
+	OpenIDEmpty      = 10004 // OpenID 为空
+)
+
+// 返回2xxxx -> MySQL错误
+const (
+	MySQLQueryError     = 20000 // MySQL的查询错误
+	MySQLScanError      = 20001 // MySQL在Scan时候的错误
+	MySQLInsertError    = 20002 // MySQL的插入错误
+	MySQLDeleteError    = 20003 // MySQL的删除错误
+	MySQLUpdateError    = 20004 // MySQL的更新错误
+	MySQLCountError     = 20004 // MySQL的计数错误
+	MySQLAffectRowError = 20005 // MySQL获取影响行数错误
+	MySQLPrepareError   = 20006
+	MySQLExecError      = 20007
+)
+
+// 返回3xxxx -> 其他类型的错误
+const (
+	StrconvAtoiError = 30000 // 类型转换错误（主要是string转int）
+	ParseTimeError   = 30001 // 日期类型的错误
+	ReadFileError    = 30002
+	UnknowError      = 30003
+)
+
+// 返回4xxxx -> json错误
+const (
+	JsonUnmarshalError = 40000
+	JsonMarshalError   = 40001
+)
+
+// 返回8xxxx -> HTTP错误
+const (
+	HTTPDoReqError  = 80000
+	HTTPNewReqError = 80001
+)
